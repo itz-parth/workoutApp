0362db4c061d8c9017d5c24bdeab4ada281d50dd
parth <parthapm2018@gmail.com>
2024-05-02 21:04:25 +0530

    dependencies

---
+++ components/Login.js
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native'
+import {LinearGradient} from 'expo-linear-gradient'
+
+const Login = () => {
+  return (
+    <LinearGradient colors={['#ff6600','#ff3300']}>
+        <Text>Hello</Text>
+    </LinearGradient>
+  )
+}
+
+export default Login
